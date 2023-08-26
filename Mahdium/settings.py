ec5e5f2c8c450c9efdccdc75d46df0a9e2bff683
matahho <mata1381@gmail.com>
2023-08-26 15:21:39 +0330

    blog added

--- Mahdium/settings.py
+++ Mahdium/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def home (request):
+    return HttpResponse("<h1>Home blog</h1>")
+
+
+def about (request):
+    return HttpResponse("<h1>ABout us</h1>")
