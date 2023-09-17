7c8d4d9076fc916a3a76aa2f12ddfb295d46a34b
matahho <mata1381@gmail.com>
2023-09-17 15:45:08 +0330

    Register Page complited

--- users/views.py
+++ users/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 
 def register (request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request , f"Welcome {username}, Your account created !")
+            return redirect('blog-home')
+        else:
+            for field, error in form.errors.items():
+                messages.warning(request , error[0])
+            
+
+    else :
+        form = UserCreationForm()
+    
     return render(request , 'users/register.html' , {'form':form} )
