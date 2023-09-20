defcd05525148e4d2c9c607beccd6ff62fb549c4
matahho <mata1381@gmail.com>
2023-09-20 14:37:06 +0330

    login page foundtaiotn added

--- Mahdium/urls.py
+++ Mahdium/urls.py
@@ -17,12 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from users import views as register_view
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('blog.urls')),
-    path('register/' , include('users.urls'))
+    path('register/' , include('users.urls') ),
+    path('login/' , auth_view.LoginView.as_view(template_name = "users/login.html"), name = "login"),
+    path('logout/' , auth_view.LogoutView.as_view() , name = "logout"),
 
 ]
 
